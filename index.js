3bbda58b86db9074060f1ecba1a8ad8a41434a8b
Shuhel Ahmed <shuhel@ymail.com>
2015-10-08 23:21:58 +0600

    feat(*): search all aurelia lib

--- index.js
+++ index.js
@@ -2,29 +2,31 @@ var fs = require('fs');
 var path = require('path');
 var globby = require('globby');
 var gs = require('glob-stream');
+var Promise = require('bluebird');
 
+var basePath = '..';
 var patterns = {
   'Object.assign': 'Object *\\. *assign *\\(',
   'Map': 'Map',
 };
+var searchArea = [
+  './src/**/*.js',
+  './test/**/*.js',
+];
 
-var stream = gs.create([
-  '../**/*.js',
-  '!../**/node_modules/**/*',
-  '!../**/jspm_packages/**/*',
-  '!../**/build/**/*',
-  '!../**/dist/**/*'
-]);
+getDirectories(basePath).forEach((d) => {
+  var opt = {
+    cwd: `${basePath}/${d}/`
+  };
 
-
-stream.on('data', (file) => {
-  searchFile(file.path);
+  var stream = gs.create(searchArea, opt);
+  stream.on('data', (file) => {
+    searchFile(file.path);
+  });
 });
 
+
 function searchFile(filePath) {
-
-  console.log(`Searching '${filePath}'`);
-
   var src = fs.readFileSync(filePath, 'utf-8');
   Object.keys(patterns).forEach(p => {
     var res, msg;
@@ -48,3 +50,9 @@ function getLineFromPos(str, pos) {
   var lines = str.substr(0, pos).match(/[\n\r]/g);
   return lines ? lines.length + 1 : 1;
 };
+
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
+}
